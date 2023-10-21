7d576388f584de364f4748b32dd9f71e3e994889
fitrat1998 <towmatovft@gmail.com>
2023-10-21 17:33:28 +0500

    dashboard header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,66 @@
 <!doctype html>
-<html>
+<html lang="en" data-layout="horizontal" data-topbar="dark" data-sidebar-size="lg" data-sidebar="light" data-sidebar-image="none" data-preloader="disable">
+
+
+<!-- Mirrored from themesbrand.com/velzon/html/saas/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 21 Oct 2023 10:25:02 GMT -->
 <head>
-    <meta charset="utf-8">
+
+    <meta charset="utf-8" />
+    <title>Dashboard | Velzon - Admin & Dashboard Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.icon') }}">
+
+    <!-- jsvectormap css -->
+    <link href="{{ asset('assets/libs/jsvectormap/css/jsvectormap.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!--Swiper slider css-->
+    <link href="{{ asset('assets/libs/swiper/swiper-bundle.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- Layout config Js -->
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet" type="text/css" />
+
 
 </head>
+
 <body>
+
     @yield('content')
+
+  <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js ') }} "></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js ') }} "></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js ') }} "></script>
+    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js ') }} "></script>
+    <script src="{{ asset('assets/js/plugins.js ') }} "></script>
+
+    <!-- apexcharts -->
+    <script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js ') }} "></script>
+
+    <!-- Vector map-->
+    <script src="{{ asset('assets/libs/jsvectormap/js/jsvectormap.min.js ') }} "></script>
+    <script src="{{ asset('assets/libs/jsvectormap/maps/world-merc.js ') }} "></script>
+
+    <!--Swiper slider js-->
+    <script src="{{ asset('assets/libs/swiper/swiper-bundle.min.js ') }} "></script>
+
+    <!-- Dashboard init -->
+    <script src="{{ asset('assets/js/pages/dashboard-ecommerce.init.js ') }} "></script>
+
+    <!-- App js -->
+    <script src="{{ asset('assets/js/app.js ') }} "></script>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,3 +11,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[PageController::class,'index'])->name('index');
 Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::get('/dashboard',[QuizController::class,'index'])->name('dashboard');
