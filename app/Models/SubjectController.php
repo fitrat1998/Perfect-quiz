c490d84d946b1f9f115f1f351bf5941afe9ef207
fitrat1998 <towmatovft@gmail.com>
2023-11-11 13:10:17 +0500

    subject

---
+++ app/Models/SubjectController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubjectController extends Model
+{
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\QuizController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -12,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[PageController::class,'index'])->name('index');
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::get('/dashboard',[QuizController::class,'index'])->name('dashboard');
+Route::get('/subject',[QuizController::class,'index'])->name('subject');
