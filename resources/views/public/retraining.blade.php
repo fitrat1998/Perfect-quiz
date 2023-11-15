42be3240330b7308e91f1c9e1a9746049fdb727e
fitrat1998 <towmatovft@gmail.com>
2023-11-15 11:10:49 +0500

    something

--- resources/views/public/retraining.blade.php
+++ resources/views/public/retraining.blade.php
@@ -17,6 +17,124 @@
                             </div>
                         </div>
 
+                        <div class="row">
+                            <h1>Mustaqil ta'lim & Test</h1>
+                        </div>
+
+                        <div class="row">
+                            <div class="col ">
+                                <div class="card rounded w-100 pt-3 border">
+
+                                     <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="106" height="119" viewBox="0 0 106 119" fill="none">
+                                          <circle cx="26.5" cy="39.5" r="68.2181" transform="rotate(46.4144 26.5 39.5)" stroke="url(#paint0_linear_319_8)" stroke-width="21"/>
+                                          <defs>
+                                            <linearGradient id="paint0_linear_319_8" x1="90.1955" y1="111.608" x2="64.9577" y2="-54.2406" gradientUnits="userSpaceOnUse">
+                                              <stop stop-color="#F3F3F3"/>
+                                              <stop offset="1" stop-color="#F1F8FF"/>
+                                            </linearGradient>
+                                          </defs>
+                                        </svg>
+                                         <span class="float-start m-1 p-2">
+                                            <h4>Matematika</h4>
+                                        </span>
+                                         <span class="float-end m-1 p-2">
+                                            <h4>Qolgan vaqt</h4>
+                                        </span>
+                                     </div>
+
+                                     <div>
+                                         <span class="float-start m-1 p-2">
+                                            <h3>30</h3>
+                                            <h6 class="text-muted">testlar soni</h6>
+                                        </span>
+                                         <span class="float-end m-1 p-2 ">
+                                            <h3 class="text-primary">14:30:41</h3>
+                                        </span>
+                                     </div>
+
+                                    
+                                </div>
+                            </div>
+                            <div class="col ">
+                                <div class="card rounded w-100 pt-3 border">
+                                     <div>
+                                         <span class="float-start m-1 p-2">
+                                            <h4>Matematika</h4>
+                                        </span>
+                                         <span class="float-end m-1 p-2">
+                                            <h4>Qolgan vaqt</h4>
+                                        </span>
+                                     </div>
+
+                                     <div>
+                                         <span class="float-start m-1 p-2">
+                                            <h3>30</h3>
+                                            <h6 class="text-muted">testlar soni</h6>
+                                        </span>
+                                         <span class="float-end m-1 p-2 ">
+                                            <h3 class="text-primary">14:30:41</h3>
+                                        </span>
+                                     </div>
+
+                                    
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="row">
+                            <div class="col ">
+                                <div class="card rounded w-100 pt-3 border">
+                                     <div>
+                                         <span class="float-start m-1 p-2">
+                                            <h4>Matematika</h4>
+                                        </span>
+                                         <span class="float-end m-1 p-2">
+                                            <h4>Qolgan vaqt</h4>
+                                        </span>
+                                     </div>
+
+                                     <div>
+                                         <span class="float-start m-1 p-2">
+                                            <h3>30</h3>
+                                            <h6 class="text-muted">testlar soni</h6>
+                                        </span>
+                                         <span class="float-end m-1 p-2 ">
+                                            <h3 class="text-primary">14:30:41</h3>
+                                        </span>
+                                     </div>
+
+                                    
+                                </div>
+                            </div>
+                            <div class="col ">
+                                <div class="card rounded w-100 pt-3 border">
+                                     <div>
+                                         <span class="float-start m-1 p-2">
+                                            <h4>Matematika</h4>
+                                        </span>
+                                         <span class="float-end m-1 p-2">
+                                            <h4>Qolgan vaqt</h4>
+                                        </span>
+                                     </div>
+
+                                     <div>
+                                         <span class="float-start m-1 p-2">
+                                            <h3>30</h3>
+                                            <h6 class="text-muted">testlar soni</h6>
+                                        </span>
+                                         <span class="float-end m-1 p-2 ">
+                                            <h3 class="text-primary">14:30:41</h3>
+                                        </span>
+                                     </div>
+
+                                    
+                                </div>
+                            </div>
+                            
+                        </div>
+
 		        </div>
 		      </div>
 		    </div>
